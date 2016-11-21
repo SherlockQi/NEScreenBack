1946293df174d2af2d80aa8a2f2864350d3a9352
sherlock <sherlock@sherlockdeAir.lan>
2016-11-21 21:05:27 +0800

    first commit

---
+++ allScreenBack/UINavigationController+screenBack.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+screenBack.h
+//  allScreenBack
+//
+//  Created by sherlock on 2016/11/21.
+//  Copyright © 2016年 xiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (screenBack)
+
+@end
---
+++ allScreenBack/UINavigationController+screenBack.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+screenBack.m
+//  allScreenBack
+//
+//  Created by sherlock on 2016/11/21.
+//  Copyright © 2016年 xiaqi. All rights reserved.
+//
+
+#import "UINavigationController+screenBack.h"
+
+@implementation UINavigationController (screenBack)
+
+@end
