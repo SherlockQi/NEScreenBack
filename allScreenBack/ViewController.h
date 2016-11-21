c9273b82b22eda48b97546de043729d5476cdedf
sherlock <sherlock@sherlockdeair.lan>
2016-11-21 20:11:30 +0800

    Initial Commit

---
+++ allScreenBack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  allScreenBack
+//
+//  Created by sherlock on 2016/11/21.
+//  Copyright © 2016年 xiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ allScreenBack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  allScreenBack
+//
+//  Created by sherlock on 2016/11/21.
+//  Copyright © 2016年 xiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
